c256f0e6f020a8c15aadd7a5d525041373ff4864
Usama Fouad <usamaeltmsah@gmail.com>
2021-03-15 12:58:07 +0200

    Dismiss keyboard when user click outside the view

--- DayPlanner/NewTaskVC.swift
+++ DayPlanner/NewTaskVC.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class NewTaskVC: UIViewController, UITextFieldDelegate {
+    @IBOutlet var TFTaskTitle: SkyFloatingLabelTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: nil)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelClicked))
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing(_:))))
+
     }
     @IBAction func goToTaskSettingClicked(_ sender: Any) {
         if let vc = storyboard?.instantiateViewController(identifier: "GlobalTaskSettings") as? GlobalTaskSettingsVC {
