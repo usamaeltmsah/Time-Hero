3525e6ae7b6a609ae1e0dd7d2bd692baad703b00
Usama Fouad <usamaeltmsah@gmail.com>
2022-11-09 18:32:28 +0200

    Add the configuration for the card cell

--- DayPlanner/Views/TableViewCells/CardsCells/DayPlanCardLeftTopOnCardTableViewCell/DayPlanCardLeftTopOnCardTableViewCell.swift
+++ DayPlanner/Views/TableViewCells/CardsCells/DayPlanCardLeftTopOnCardTableViewCell/DayPlanCardLeftTopOnCardTableViewCell.swift
@@ -9,15 +9,39 @@ import UIKit
 
 class DayPlanCardLeftTopOnCardTableViewCell: UITableViewCell {
 
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var leftFromTimeLabel: UILabel!
+    @IBOutlet var left1TimeSeperator: UILabel!
+    @IBOutlet var left2TimeSeperator: UIImageView!
+    @IBOutlet var leftToTimeLabel: UILabel!
+    @IBOutlet var topFromTimeLabel: UILabel!
+    @IBOutlet var topFromTimeSeperator: UILabel!
+    @IBOutlet var topToTimeLabel: UILabel!
+    @IBOutlet var onCardFromTimeLabel: UILabel!
+    @IBOutlet var onCardTimeSeperator: UILabel!
+    @IBOutlet var onCardToTimeLabel: UILabel!
+    @IBOutlet var taskTitleLabel: UILabel!
+    @IBOutlet var taskCatLabel: UILabel!
+    @IBOutlet var taskDescLabel: UILabel!
+    @IBOutlet var taskLengthLabel: UILabel!
+    @IBOutlet var leftCardTimeView: UIView!
+    @IBOutlet var topCardTimeView: UIView!
+    @IBOutlet var onCardTimeView: UIStackView!
+    @IBOutlet var cardContentStackView: UIStackView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        setupUI()
     }
     
+    private func setupUI() {
+        left1TimeSeperator.font = UIFont(name: "Poppins-Medium", size: 12)
+        
+        topFromTimeLabel.font = UIFont(name: "Poppins-Bold", size: 12)
+        topFromTimeSeperator.font = UIFont(name: "Poppins-Bold", size: 12)
+        topToTimeLabel.font = UIFont(name: "Poppins-Bold", size: 12)
+        
+        onCardFromTimeLabel.font = UIFont(name: "Poppins-SemiBold", size: 16)
+        onCardTimeSeperator.font = UIFont(name: "Poppins-SemiBold", size: 16)
+        onCardToTimeLabel.font = UIFont(name: "Poppins-SemiBold", size: 16)
+    }
 }
